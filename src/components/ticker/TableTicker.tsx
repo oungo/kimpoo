2b36d963109079a29af637ee05c5592c369d6dfa
oungo <y2k6273@naver.com>
2023-03-06 19:12:56 +0900

    feat: change table heads

--- src/components/ticker/TableTicker.tsx
+++ src/components/ticker/TableTicker.tsx
@@ -14,9 +14,8 @@ const TableTicker = () => {
           <th>이름</th>
           <th>현재가</th>
           <th>김프</th>
-          <th>전일 대비 값</th>
-          <th>누적 거래 대금</th>
-          <th>유의 종목 여부</th>
+          <th>전일 대비</th>
+          <th>유의 종목</th>
           <th>거래액(일)</th>
         </tr>
       </thead>
