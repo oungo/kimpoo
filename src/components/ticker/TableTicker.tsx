16046276d8d437e6ee785646cc140b054ca0ecdb
oungo <y2k6273@naver.com>
2023-03-08 21:03:04 +0900

    style: change table layout

--- src/components/ticker/TableTicker.tsx
+++ src/components/ticker/TableTicker.tsx
@@ -8,14 +8,14 @@ const TableTicker = () => {
   });
 
   return (
-    <table className="table-fixed text-sm">
+    <table className="table-fixed text-sm w-full max-w-screen-lg">
       <thead>
         <tr className="text-right border-b border-b-gray-500">
           <th className="w-20 text-left">이름</th>
-          <th className="w-28">현재가</th>
-          <th className="w-20">김프</th>
-          <th className="w-20">전일 대비</th>
-          <th className="w-24">거래액(일)</th>
+          <th className="w-20">현재가</th>
+          <th className="w-16">김프</th>
+          <th className="w-16">전일 대비</th>
+          <th className="w-16">거래액(일)</th>
         </tr>
       </thead>
       <TableTickerBody quotation={quotation?.[0].basePrice} />
