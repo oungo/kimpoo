0e94514ad29713f36e1059cf08c54686bcfe4a86
oungo <y2k6273@naver.com>
2023-03-18 20:42:26 +0900

    feat: add useTickerList hook

---
+++ src/components/ticker/hooks/useTickerList.ts
@@ -0,0 +1,6 @@
+import { useTickerStore } from 'store/useTickerStore';
+
+export const useTickerList = () => {
+  const ticker = useTickerStore((state) => state.tickerList);
+  return Array.from(ticker.values()).filter((t) => t.symbol);
+};
