647a1fbe739e55a6398fb9a6bdbb09904922d793
oungo <y2k6273@naver.com>
2023-03-19 21:50:12 +0900

    feat: add ExchangeSelectGroup component

---
+++ src/components/select/ExchangeSelectGroup.tsx
@@ -0,0 +1,21 @@
+import { useTickerStore } from 'store/useTickerStore';
+import { Select, Option } from '.';
+import { DomesticExchangeList } from '../ticker/types';
+
+const ExchangeSelectGroup = () => {
+  const setDomesticExchange = useTickerStore((state) => state.setDomesticExchange);
+
+  const handleSelect = (option: DomesticExchangeList) => {
+    setDomesticExchange(option);
+  };
+
+  return (
+    <Select defaultValue={DomesticExchangeList.UPBIT} onSelect={handleSelect}>
+      <Option value={DomesticExchangeList.UPBIT}>업비트</Option>
+      <Option value={DomesticExchangeList.BITTHUMB}>빗썸</Option>
+      <Option value={DomesticExchangeList.COINONE}>코인원</Option>
+    </Select>
+  );
+};
+
+export default ExchangeSelectGroup;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import { Select, Option } from '@/components/select';
+import ExchangeSelectGroup from '@/components/select/ExchangeSelectGroup';
 import TableTicker from '@/components/ticker/TableTicker';
 
 const index = () => {
   return (
     <div className="p-3">
-      <Select defaultValue="업비트">
-        <Option value="빗썸">빗썸</Option>
-        <Option value="코인원">코인원</Option>
-      </Select>
+      <ExchangeSelectGroup />
       <TableTicker />
     </div>
   );
