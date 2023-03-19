3fc4bdfa7ee4863249f50affdf5aa0826eaa9ca7
oungo <y2k6273@naver.com>
2023-03-19 21:49:27 +0900

    feat: add domesticExchange state in store

--- src/store/useTickerStore.ts
+++ src/store/useTickerStore.ts
@@ -1,9 +1,16 @@
-import { DomesticTicker, OverseasTicker, Ticker } from '@/components/ticker/types';
+import {
+  DomesticExchangeList,
+  DomesticTicker,
+  OverseasTicker,
+  Ticker,
+} from '@/components/ticker/types';
 import { create } from 'zustand';
 
 interface TickerState {
   tickerList: Map<Ticker['symbol'], Ticker>;
   setTickerList: (symbol: string, tickerList: DomesticTicker | OverseasTicker) => void;
+  domesticExchange: DomesticExchangeList;
+  setDomesticExchange: (exchange: DomesticExchangeList) => void;
 }
 
 export const useTickerStore = create<TickerState>()((set) => ({
@@ -13,4 +20,6 @@ export const useTickerStore = create<TickerState>()((set) => ({
       tickerList: new Map(tickerList).set(symbol, { ...tickerList.get(symbol), ...ticker }),
     }));
   },
+  domesticExchange: DomesticExchangeList.UPBIT,
+  setDomesticExchange: (exchange) => set({ domesticExchange: exchange }),
 }));
