060af360c3d04ee2a9b7c06877a11fd56b518d69
oungo <y2k6273@naver.com>
2023-03-30 00:26:33 +0900

    feat: convert bithumb data

--- src/api/fetchBithumbMarket.ts
+++ src/api/fetchBithumbMarket.ts
@@ -1,9 +1,10 @@
 interface BithumbMarket {
   status: string;
-  data: Record<string, BithumbResponse>;
+  data: BithumbTicker[];
 }
 
-interface BithumbResponse {
+interface BithumbTicker {
+  symbol: string;
   acc_trade_value: string;
   acc_trade_value_24H: string;
   closing_price: string;
@@ -17,12 +18,24 @@ interface BithumbResponse {
   units_traded_24H: string;
 }
 
+type JSONResponse = {
+  status: string;
+  data: Record<string, BithumbTicker>;
+};
+
 export const fetchBithumbMarket = async (): Promise<BithumbMarket> => {
   const response = await fetch('https://api.bithumb.com/public/ticker/ALL_KRW');
 
   if (response.ok) {
-    return response.json();
+    const { status, data }: JSONResponse = await response.json();
+
+    const newMarket: BithumbMarket['data'] = [];
+    for (const symbol in data) {
+      newMarket.push({ symbol, ...data[symbol] });
+    }
+
+    return { status, data: newMarket };
   }
 
-  return Promise.reject(new Error('Failed fetch upbit market.'));
+  return Promise.reject(new Error('Failed fetch bithumb market.'));
 };
