4cc29ea92a0cb259cc9a1e6aed8b27251c3ec9af
oungo <y2k6273@naver.com>
2023-03-30 23:25:55 +0900

    feat: implement darkmode

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,20 +1,46 @@
 import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 
 interface Props {
   children: ReactNode;
 }
-const Layout = ({ children }: Props) => (
-  <>
-    <header className="p-2">header</header>
+const Layout = ({ children }: Props) => {
+  useEffect(() => {
+    if (
+      localStorage.getItem('theme') === 'dark' ||
+      (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
+    }
+  }, []);
 
-    <nav className="flex justify-between p-2">
-      <p>home</p>
-      <button>다크모드</button>
-    </nav>
+  const handleClickButton = () => {
+    if (localStorage.getItem('theme') === 'dark') {
+      localStorage.setItem('theme', 'light');
+      document.documentElement.classList.remove('dark');
+    } else {
+      localStorage.setItem('theme', 'dark');
+      document.documentElement.classList.add('dark');
+    }
+  };
 
-    <main className="max-w-screen-lg p-2 m-auto">{children}</main>
-    <footer>footer</footer>
-  </>
-);
+  return (
+    <>
+      <header className="p-2 dark:bg-red-400">header</header>
+
+      <nav className="flex justify-between p-2">
+        <p>home</p>
+        <button onClick={handleClickButton}>다크모드</button>
+      </nav>
+
+      <main className="max-w-screen-lg p-2 m-auto">{children}</main>
+      <footer>footer</footer>
+    </>
+  );
+};
 
 export default Layout;
