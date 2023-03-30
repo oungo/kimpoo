1e3ea09633adf4945707ad93cb19841046b144ef
oungo <y2k6273@naver.com>
2023-03-30 22:50:46 +0900

    feat: add default layout markup

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -4,7 +4,17 @@ interface Props {
   children: ReactNode;
 }
 const Layout = ({ children }: Props) => (
-  <main className="max-w-screen-lg p-2 m-auto">{children}</main>
+  <>
+    <header className="p-2">header</header>
+
+    <nav className="flex justify-between p-2">
+      <p>home</p>
+      <button>다크모드</button>
+    </nav>
+
+    <main className="max-w-screen-lg p-2 m-auto">{children}</main>
+    <footer>footer</footer>
+  </>
 );
 
 export default Layout;
