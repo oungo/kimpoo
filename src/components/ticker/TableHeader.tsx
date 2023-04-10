6d07c2a1f9af8fe6c3162b10e45e2ccd85bce83e
oungo <y2k6273@naver.com>
2023-04-11 06:20:57 +0900

    style: add style for darkmode

--- src/components/ticker/TableHeader.tsx
+++ src/components/ticker/TableHeader.tsx
@@ -15,19 +15,25 @@ const TableHeader = ({ sortType, onSort, children }: Props) => {
     <th
       onClick={onSort}
       className={`py-2 cursor-pointer ${sortType === 'symbol' ? 'text-left' : 'text-right'} ${
-        sortOption.type === sortType ? '!text-white' : 'text-neutral-500'
+        sortOption.type === sortType
+          ? '!text-black dark:!text-white'
+          : 'text-neutral-400 dark:text-neutral-500'
       }`}
     >
       {children}
       <span className="ml-1 text-[10px] relative">
         <i
           className={`absolute -top-[2px] fa-solid fa-caret-up ${
-            sortOption.type === sortType && !sortOption.desc ? 'text-white' : 'text-neutral-500'
+            sortOption.type === sortType && !sortOption.desc
+              ? 'text-black dark:text-white'
+              : 'text-neutral-400 dark:text-neutral-500'
           }`}
         />
         <i
           className={`absolute -bottom-[2px] fa-solid fa-caret-down ${
-            sortOption.type === sortType && sortOption.desc ? 'text-white' : 'text-neutral-500'
+            sortOption.type === sortType && sortOption.desc
+              ? 'text-black dark:text-white'
+              : 'text-neutral-400 dark:text-neutral-500'
           } `}
         />
       </span>
