85d157eb60b922bf110b985e3b78de8d5e11ee8e
oungo <y2k6273@naver.com>
2023-04-12 23:57:12 +0900

    feat: format changePrice minimumFractionDigits

--- src/components/ticker/TickerItem.tsx
+++ src/components/ticker/TickerItem.tsx
@@ -52,7 +52,7 @@ const TickerItem = ({ ticker, koreanSymbol }: Props) => {
       </td>
 
       <td className={ticker.changeRate > 0 ? 'text-teal-600' : 'text-red-600 dark:text-red-500'}>
-        {formatPrice(ticker.changeRate, { signDisplay: 'exceptZero' })}%
+        {formatPrice(ticker.changeRate, { signDisplay: 'exceptZero', minimumFractionDigits: 2 })}%
       </td>
 
       <td className="flex flex-col">
