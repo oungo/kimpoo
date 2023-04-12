ebcceb35534d5bbb8a54694ccd9428023ec9d6b8
oungo <y2k6273@naver.com>
2023-04-12 21:19:29 +0900

    feat: add meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,10 @@ const App = ({ Component, pageProps }: AppProps<PageProps>) => {
           content="김프,김치프리미엄,암호화폐,가상화폐,비트코인,이더리움,비트,이더,코인,역프,업비트,빗썸,바이낸스,binance,bitcoin,ethereum,btc,eth"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="subject" content="암호화폐 시세 및 김치프리미엄을 실시간 제공합니다." />
+        <meta name="description" content="암호화폐 시세 및 김치프리미엄을 실시간 제공합니다. " />
+        <meta name="author" content="oungo" />
+        <meta name="robots" content="index,follow" />
       </Head>
       <Script
         src="https://kit.fontawesome.com/110e54d917.js"
