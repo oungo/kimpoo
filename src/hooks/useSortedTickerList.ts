ea9cb41946b712abfce2884ae310c72f4e18e4be
oungo <y2k6273@naver.com>
2023-04-15 15:19:48 +0900

    feat: simplify sort tickerList

--- src/hooks/useSortedTickerList.ts
+++ src/hooks/useSortedTickerList.ts
@@ -5,38 +5,32 @@ import { useEffect, useState } from 'react';
 export const useSortedTickerList = () => {
   const [sortedTickerList, setSortedTickerList] = useState<Ticker[]>([]);
 
-  const tickerList = useTickerStore((state) => state.tickerMap);
+  const tickerMap = useTickerStore((state) => state.tickerMap);
   const { type, desc } = useTickerStore((state) => state.sortOption);
 
   useEffect(() => {
-    const newTickerList = [...tickerList.values()];
+    const tickerList = [...tickerMap.values()];
 
     switch (type) {
       case 'symbol':
-        newTickerList.sort((a, b) => {
+        tickerList.sort((a, b) => {
           if (desc) return a.symbol > b.symbol ? -1 : b.symbol > a.symbol ? 1 : 0;
           return a.symbol < b.symbol ? -1 : b.symbol > a.symbol ? 1 : 0;
         });
         break;
       case 'premium':
-        newTickerList.sort((a, b) => {
-          if (!b.oCurrentPrice || !b.premium) return -1;
+        tickerList.sort((a, b) => {
           if (!a.premium) return 1;
-          if (desc) return b.premium - a.premium;
-          return a.premium - b.premium;
+          if (!b.premium) return -1;
+          return desc ? b.premium - a.premium : a.premium - b.premium;
         });
         break;
-      case 'changeRate':
-      case 'currentPrice':
-      case 'transactionAmount':
-        newTickerList.sort((a, b) => {
-          if (desc) return b[type] - a[type];
-          return a[type] - b[type];
-        });
+      default:
+        tickerList.sort((a, b) => (desc ? b[type] - a[type] : a[type] - b[type]));
     }
 
-    setSortedTickerList(newTickerList);
-  }, [desc, type, tickerList]);
+    setSortedTickerList(tickerList);
+  }, [desc, type, tickerMap]);
 
   return sortedTickerList;
 };
