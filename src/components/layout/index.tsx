71c4ae5df6e9451d2984e5d5bab554e487a401c7
oungo <y2k6273@naver.com>
2023-04-20 08:48:52 +0900

    feat: add inquiry info in footer

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -72,9 +72,21 @@ const Layout = ({ children }: Props) => {
 
       <footer className="relative bottom-0 w-full h-40 mt-10 bg-gray-200 dark:bg-neutral-800">
         <div className="max-w-screen-lg p-5 m-auto text-xs sm:text-sm">
-          KIMPOO에서 제공하는 정보는 투자에 대한 조언이 아닙니다.
-          <br />
-          투자에 대한 책임은 전적으로 본인에게 있습니다.
+          <p className="whitespace-pre-line">
+            KIMPOO에서 제공하는 정보는 투자에 대한 조언이 아닙니다.{'\n'}투자에 대한 책임은 전적으로
+            본인에게 있습니다.
+          </p>
+          <div className="mt-5">
+            문의
+            <a
+              className="ml-2 font-bold"
+              target="_blank"
+              rel="noreferrer"
+              href="http://t.me/kimpuu"
+            >
+              http://t.me/kimpuu
+            </a>
+          </div>
         </div>
       </footer>
     </>
