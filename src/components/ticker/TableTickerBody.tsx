eee051d51ab7467f3975ed0bc6c6a6e576db59a6
oungo <y2k6273@naver.com>
2023-04-20 08:51:10 +0900

    feat: pass thumb, symbolName props

--- src/components/ticker/TableTickerBody.tsx
+++ src/components/ticker/TableTickerBody.tsx
@@ -3,11 +3,14 @@ import { useBithumbTickers } from '@/hooks/useBithumbTickers';
 import { useSortedTickerList } from '@/hooks/useSortedTickerList';
 import { useUpbitMarketQuery } from '@/hooks/useUpbitMarketQuery';
 import { useUpbitTickers } from '@/hooks/useUpbitTickers';
+import { useTickerStore } from '@/store/useTickerStore';
 import TickerItem from './TickerItem';
 
 const TableTickerBody = () => {
-  const tickerList = useSortedTickerList();
+  const coinList = useTickerStore((state) => state.coinList);
+  const domesticExchange = useTickerStore((state) => state.domesticExchange);
 
+  const tickerList = useSortedTickerList();
   const { data: upbitMarket } = useUpbitMarketQuery();
 
   useUpbitTickers();
@@ -20,7 +23,15 @@ const TableTickerBody = () => {
         <TickerItem
           key={ticker.symbol}
           ticker={ticker}
-          symbolName={upbitMarket?.find(({ market }) => market === ticker.symbol)?.korean_name}
+          thumb={
+            domesticExchange.startsWith('UPBIT')
+              ? `https://static.upbit.com/logos/${ticker.symbol}.png`
+              : coinList.get(ticker.symbol)?.thumb
+          }
+          symbolName={
+            upbitMarket?.find(({ market }) => market === ticker.symbol)?.korean_name ||
+            coinList.get(ticker.symbol)?.name
+          }
         />
       ))}
     </tbody>
--- src/components/ticker/TickerItem.tsx
+++ src/components/ticker/TickerItem.tsx
@@ -1,4 +1,3 @@
-import { useTickerStore } from '@/store/useTickerStore';
 import { formatCurrentPrice, formatNumber, formatPercent } from '@/utils/common';
 import type { Ticker } from './types';
 import Image from 'next/image';
@@ -6,74 +5,74 @@ import { memo } from 'react';
 
 interface Props {
   ticker: Ticker;
+  thumb?: string;
   symbolName?: string;
 }
 
-const TickerItem = ({ ticker, symbolName }: Props) => {
-  const coinList = useTickerStore((state) => state.coinList);
-
-  return (
-    <tr className="text-right border-b-gray-200 border-b tracking-tight [&>td]:py-1 dark:border-b-neutral-700">
-      <td className="text-left">
-        <div className="flex items-center gap-1">
+const TickerItem = ({ ticker, thumb, symbolName }: Props) => (
+  <tr className="text-right border-b-gray-200 border-b tracking-tight [&>td]:py-1 dark:border-b-neutral-700">
+    <td className="text-left">
+      <div className="flex items-center gap-1">
+        {thumb ? (
           <Image
-            src={coinList.get(ticker.symbol)?.thumb || ''}
+            src={thumb}
             alt={`${ticker.symbol} 아이콘`}
             width={15}
             height={15}
             className="rounded-full"
           />
-          <p className="overflow-hidden whitespace-nowrap text-ellipsis">
-            {symbolName || coinList.get(ticker.symbol)?.name}
-          </p>
-        </div>
-        <div className="flex items-center gap-1">
-          <p className="inline-block text-gray-500 dark:text-gray-400">{ticker.symbol}</p>
-          {ticker.caution && <i className="text-yellow-400 fa-solid fa-circle-exclamation" />}
-        </div>
-      </td>
+        ) : (
+          <div className="w-[15px] h-[15px] rounded-full bg-gray-500 dark:bg-gray-400" />
+        )}
 
-      <td className="flex flex-col">
-        <p>{formatCurrentPrice(ticker.currentPrice)}</p>
-        <p className="text-gray-500 transition-opacity dark:text-gray-400">
-          {ticker.oCurrentPrice ? formatCurrentPrice(ticker.oCurrentPrice) : '-'}
-        </p>
-      </td>
+        <p className="overflow-hidden whitespace-nowrap text-ellipsis">{symbolName}</p>
+      </div>
+      <div className="flex items-center gap-1">
+        <p className="inline-block text-gray-500 dark:text-gray-400">{ticker.symbol}</p>
+        {ticker.caution && <i className="text-yellow-400 fa-solid fa-circle-exclamation" />}
+      </div>
+    </td>
 
-      <td
-        className={
-          !ticker.premium
-            ? 'dark:text-gray-400'
-            : ticker.premium > 0
-            ? 'text-teal-700 dark:text-teal-600'
-            : 'text-red-600 dark:text-red-500'
-        }
-      >
-        {ticker.premium ? `${formatPercent(ticker.premium)}` : '-'}
-      </td>
+    <td className="flex flex-col">
+      <p>{formatCurrentPrice(ticker.currentPrice)}</p>
+      <p className="text-gray-500 transition-opacity dark:text-gray-400">
+        {ticker.oCurrentPrice ? formatCurrentPrice(ticker.oCurrentPrice) : '-'}
+      </p>
+    </td>
 
-      <td
-        className={
-          !ticker.changeRate
-            ? 'dark:text-white'
-            : ticker.changeRate > 0
-            ? 'text-teal-700 dark:text-teal-600'
-            : 'text-red-600 dark:text-red-500'
-        }
-      >
-        {ticker.changeRate && formatPercent(ticker.changeRate)}
-      </td>
+    <td
+      className={
+        !ticker.premium
+          ? 'dark:text-gray-400'
+          : ticker.premium > 0
+          ? 'text-teal-700 dark:text-teal-600'
+          : 'text-red-600 dark:text-red-500'
+      }
+    >
+      {ticker.premium ? `${formatPercent(ticker.premium)}` : '-'}
+    </td>
 
-      <td className="flex flex-col">
-        <p>{formatNumber(ticker.transactionAmount, { notation: 'compact' })}</p>
+    <td
+      className={
+        !ticker.changeRate
+          ? 'dark:text-white'
+          : ticker.changeRate > 0
+          ? 'text-teal-700 dark:text-teal-600'
+          : 'text-red-600 dark:text-red-500'
+      }
+    >
+      {ticker.changeRate && formatPercent(ticker.changeRate)}
+    </td>
 
-        <p className="text-gray-500 dark:text-gray-400">
-          {ticker.oTransactionAmount
-            ? formatNumber(ticker.oTransactionAmount, { notation: 'compact' })
-            : '-'}
-        </p>
-      </td>
-    </tr>
-  );
-};
+    <td className="flex flex-col">
+      <p>{formatNumber(ticker.transactionAmount, { notation: 'compact' })}</p>
+
+      <p className="text-gray-500 dark:text-gray-400">
+        {ticker.oTransactionAmount
+          ? formatNumber(ticker.oTransactionAmount, { notation: 'compact' })
+          : '-'}
+      </p>
+    </td>
+  </tr>
+);
 export default memo(TickerItem);
