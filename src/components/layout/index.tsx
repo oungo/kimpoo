a8842eef53a1eac26028d2e1b9ba7df2afb9d018
oungo <y2k6273@naver.com>
2023-04-20 00:36:18 +0900

    feat: change footer text

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -71,7 +71,11 @@ const Layout = ({ children }: Props) => {
       <main className="max-w-screen-lg min-h-screen px-2 py-4 m-auto">{children}</main>
 
       <footer className="relative bottom-0 w-full h-40 mt-10 bg-gray-200 dark:bg-neutral-800">
-        footer
+        <div className="max-w-screen-lg p-5 m-auto text-xs sm:text-sm">
+          KIMPOO에서 제공하는 정보는 투자에 대한 조언이 아닙니다.
+          <br />
+          투자에 대한 책임은 전적으로 본인에게 있습니다.
+        </div>
       </footer>
     </>
   );
