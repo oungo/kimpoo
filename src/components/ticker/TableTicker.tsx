2aa4784076cbe0df63459e65b9663f9de216204a
oungo <y2k6273@naver.com>
2023-04-23 13:25:59 +0900

    feat: remove unnecessary handler

--- src/components/ticker/TableTicker.tsx
+++ src/components/ticker/TableTicker.tsx
@@ -1,14 +1,9 @@
 import { useSortTicker } from '@/hooks/useSortTicker';
-import type { SortType } from '@/store/useTickerStore';
 import TableHeader from './TableHeader';
 import TableTickerBody from './TableTickerBody';
 
 const TableTicker = () => {
   const { sortTicker } = useSortTicker();
-
-  const handleSortClick = (type: SortType) => {
-    sortTicker(type);
-  };
 
   return (
     <table className="w-full max-w-screen-lg mt-4 text-xs table-fixed sm:text-sm">
@@ -22,22 +17,19 @@ const TableTicker = () => {
 
       <thead className="text-xs">
         <tr className="text-right border-b border-b-gray-500 dark:border-b-neutral-700 [&>th]:text-neutral-500">
-          <TableHeader onSort={() => handleSortClick('symbol')} sortType="symbol">
+          <TableHeader onSort={() => sortTicker('symbol')} sortType="symbol">
             이름
           </TableHeader>
-          <TableHeader onSort={() => handleSortClick('currentPrice')} sortType="currentPrice">
+          <TableHeader onSort={() => sortTicker('currentPrice')} sortType="currentPrice">
             현재가
           </TableHeader>
-          <TableHeader onSort={() => handleSortClick('premium')} sortType="premium">
+          <TableHeader onSort={() => sortTicker('premium')} sortType="premium">
             김프
           </TableHeader>
-          <TableHeader onSort={() => handleSortClick('changeRate')} sortType="changeRate">
+          <TableHeader onSort={() => sortTicker('changeRate')} sortType="changeRate">
             전일 대비
           </TableHeader>
-          <TableHeader
-            onSort={() => handleSortClick('transactionAmount')}
-            sortType="transactionAmount"
-          >
+          <TableHeader onSort={() => sortTicker('transactionAmount')} sortType="transactionAmount">
             거래액(일)
           </TableHeader>
         </tr>
