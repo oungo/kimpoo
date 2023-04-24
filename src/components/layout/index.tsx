c22e5c38c4f81b87dabaf1d3be423cd5eed74887
oungo <y2k6273@naver.com>
2023-04-25 00:31:02 +0900

    feat: create Footer component

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,3 +1,4 @@
+import Footer from './Footer';
 import Header from './Header';
 import Nav from './Nav';
 import type { ReactNode } from 'react';
@@ -14,20 +15,7 @@ const Layout = ({ children }: Props) => (
 
     <main className="max-w-screen-lg min-h-screen px-2 py-4 m-auto">{children}</main>
 
-    <footer className="relative bottom-0 w-full h-40 mt-10 bg-gray-200 dark:bg-neutral-800">
-      <div className="max-w-screen-lg p-5 m-auto text-xs sm:text-sm">
-        <p className="whitespace-pre-line">
-          KIMPOO에서 제공하는 정보는 투자에 대한 조언이 아닙니다.{'\n'}투자에 대한 책임은 전적으로
-          본인에게 있습니다.
-        </p>
-        <div className="mt-5">
-          문의
-          <a className="ml-2 font-bold" target="_blank" rel="noreferrer" href="http://t.me/kimpuu">
-            http://t.me/kimpuu
-          </a>
-        </div>
-      </div>
-    </footer>
+    <Footer />
   </>
 );
 
