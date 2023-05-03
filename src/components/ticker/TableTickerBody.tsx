8d2d6538f5e8c64f357ae66674c3a7b21ba34e09
oungo <y2k6273@naver.com>
2023-05-03 22:19:51 +0900

    style: change loading image

--- src/components/ticker/TableTickerBody.tsx
+++ src/components/ticker/TableTickerBody.tsx
@@ -56,7 +56,7 @@ const TableTickerBody = () => {
 const Loading = () => (
   <tr className="relative">
     <td className="absolute mt-5 text-center -translate-x-1/2 left-1/2">
-      <i className="text-5xl fa-solid fa-spinner fa-spin sm:text-7xl" />
+      <i className="text-5xl fa-solid fa-circle-notch fa-spin sm:text-7xl" />
     </td>
   </tr>
 );
