2b7d83dfab9b51151d7fabab7696b50c9b7b74bc
oungo <y2k6273@naver.com>
2023-05-05 10:31:07 +0900

    feat: create SearchInput component

---
+++ src/components/shared/SearchInput.tsx
@@ -0,0 +1,19 @@
+import type { ChangeEvent } from 'react';
+
+interface Props {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onClear: () => void;
+}
+
+const SearchInput = ({ value, onChange, onClear }: Props) => {
+  return (
+    <div className="relative inline-block">
+      <i className="absolute -translate-y-1/2 top-1/2 fa-solid fa-magnifying-glass left-2" />
+      <input value={value} onChange={onChange} className="px-8 py-2 rounded-md bg-neutral-800 focus:outline-none" />
+      {value && <i onClick={onClear} className="absolute -translate-y-1/2 right-2 fa-solid fa-xmark top-1/2" />}
+    </div>
+  );
+};
+
+export default SearchInput;
