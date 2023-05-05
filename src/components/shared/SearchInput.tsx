8df026e545343c53d26a5f044b4b277e315ecf7b
oungo <y2k6273@naver.com>
2023-05-05 11:31:26 +0900

    style: change input style

--- src/components/shared/SearchInput.tsx
+++ src/components/shared/SearchInput.tsx
@@ -10,7 +10,11 @@ const SearchInput = ({ value, onChange, onClear }: Props) => {
   return (
     <div className="relative inline-block">
       <i className="absolute -translate-y-1/2 top-1/2 fa-solid fa-magnifying-glass left-2" />
-      <input value={value} onChange={onChange} className="px-8 py-2 rounded-md bg-neutral-800 focus:outline-none" />
+      <input
+        value={value}
+        onChange={onChange}
+        className="w-40 px-8 py-2 text-sm rounded-md bg-neutral-800 focus:outline-none"
+      />
       {value && <i onClick={onClear} className="absolute -translate-y-1/2 right-2 fa-solid fa-xmark top-1/2" />}
     </div>
   );
