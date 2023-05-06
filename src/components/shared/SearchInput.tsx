ab7580214d8668d35eaa76193150220d81084d4e
oungo <y2k6273@naver.com>
2023-05-06 23:30:39 +0900

    feat: add searchInput placeholder

--- src/components/shared/SearchInput.tsx
+++ src/components/shared/SearchInput.tsx
@@ -13,7 +13,8 @@ const SearchInput = ({ value, onChange, onClear }: Props) => {
       <input
         value={value}
         onChange={onChange}
-        className="w-40 px-8 py-2 text-sm rounded-md bg-neutral-800 focus:outline-none"
+        placeholder="비트코인, btc"
+        className="w-40 px-8 py-2 text-sm border border-gray-200 rounded-md dark:bg-neutral-800 focus:outline-none dark:border-neutral-700 sm:w-60"
       />
       {value && <i onClick={onClear} className="absolute -translate-y-1/2 right-2 fa-solid fa-xmark top-1/2" />}
     </div>
