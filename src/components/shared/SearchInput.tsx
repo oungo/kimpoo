9c3d527b006d1e889eaf21f03e3f8a0236682f66
oungo <y2k6273@naver.com>
2023-05-10 09:15:31 +0900

    style: change input font size

--- src/components/shared/SearchInput.tsx
+++ src/components/shared/SearchInput.tsx
@@ -14,7 +14,7 @@ const SearchInput = ({ value, onChange, onClear }: Props) => {
         value={value}
         onChange={onChange}
         placeholder="비트코인, btc"
-        className="w-40 px-8 py-2 text-sm border border-gray-200 rounded-md dark:bg-neutral-800 focus:outline-none dark:border-neutral-700 sm:w-60"
+        className="w-40 px-8 py-2 border border-gray-200 rounded-md dark:bg-neutral-800 focus:outline-none dark:border-neutral-700 sm:w-60"
       />
       {value && <i onClick={onClear} className="absolute -translate-y-1/2 right-2 fa-solid fa-xmark top-1/2" />}
     </div>
