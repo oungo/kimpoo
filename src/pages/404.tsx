36f431bdebb62872efff6ffc6cf120d32c82fcb7
oungo <y2k6273@naver.com>
2023-05-11 20:16:50 +0900

    feat: add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const Custom404 = () => {
+  return (
+    <div className="px-2 mt-40 text-center sm:mt-80">
+      <h1 className="text-9xl">404</h1>
+      <h2 className="mb-20 text-xl">존재하지 않는 페이지입니다.</h2>
+      <Link href="/" className="px-4 py-2 border rounded-md">
+        GO TO HOMEPAGE
+      </Link>
+    </div>
+  );
+};
+
+export default Custom404;
