bec0f8a1702a98edb3fc8da2ccba2b64a9c79cdb
oungo <y2k6273@naver.com>
2023-05-11 23:58:54 +0900

    style: add clear input cursor

--- src/components/shared/SearchInput.tsx
+++ src/components/shared/SearchInput.tsx
@@ -16,7 +16,12 @@ const SearchInput = ({ value, onChange, onClear }: Props) => {
         placeholder="비트코인, btc"
         className="w-40 px-8 py-2 border border-gray-200 rounded-md dark:bg-neutral-800 focus:outline-none dark:border-neutral-700 sm:w-60"
       />
-      {value && <i onClick={onClear} className="absolute -translate-y-1/2 right-2 fa-solid fa-xmark top-1/2" />}
+      {value && (
+        <i
+          onClick={onClear}
+          className="absolute p-2 -translate-y-1/2 cursor-pointer right-2 fa-solid fa-xmark top-1/2"
+        />
+      )}
     </div>
   );
 };
