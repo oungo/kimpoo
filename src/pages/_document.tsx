fa91882751a4b0c3f17dfeca2e191d716a18002d
oungo <y2k6273@naver.com>
2023-05-12 01:01:25 +0900

    fix: fix darkmode flicking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <Script src="/js/theme.js" strategy="beforeInteractive" />
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script src="/js/theme.js" />
       </Head>
       <body>
         <Main />
