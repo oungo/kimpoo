4a9dd81c147710a0da13c562fe5d9523222d309d
oungo <y2k6273@naver.com>
2023-05-17 20:20:18 +0900

    chore: add gtag script-src

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const ContentSecurityPolicy = `
   default-src 'self' https: wss:;
-  script-src 'self' https://kit.fontawesome.com 'unsafe-eval';
+  script-src 'self' https://kit.fontawesome.com https://www.googletagmanager.com 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
   font-src 'self' https:;
 `;
