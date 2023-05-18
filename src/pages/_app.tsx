ac4bb3d8a229451a524c3819da1b6523cd70b729
oungo <y2k6273@naver.com>
2023-05-18 19:36:46 +0900

    feat: add favicon.ico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -64,6 +64,7 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
         <meta property="og:site_name" content="KIMPUU - 김치프리미엄 암호화폐 시세" />
         <meta property="og:locale" content="ko_KR" />
 
+        <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
