8219f626b04dfc38901b9dae5942e8603aec3c54
oungo <y2k6273@naver.com>
2023-05-18 19:42:03 +0900

    feat: add 500 error page

---
+++ src/pages/500.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const Custom500 = () => {
+  return (
+    <div className="px-2 mt-40 text-center sm:mt-80">
+      <h1 className="text-9xl">500</h1>
+      <h2 className="mb-20 text-xl">Internal Server Error</h2>
+      <Link href="/" className="px-4 py-2 border rounded-md">
+        GO TO HOMEPAGE
+      </Link>
+    </div>
+  );
+};
+
+export default Custom500;
