9780da4ac2626b3b134710a70e8c5a8aba3021d0
oungo <y2k6273@naver.com>
2023-05-18 19:44:03 +0900

    feat: change 500 page text

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -4,7 +4,8 @@ const Custom500 = () => {
   return (
     <div className="px-2 mt-40 text-center sm:mt-80">
       <h1 className="text-9xl">500</h1>
-      <h2 className="mb-20 text-xl">Internal Server Error</h2>
+      <h2 className="text-xl">Internal Server Error</h2>
+      <h2 className="mb-20 text-xl">잠시 후 다시 시도해주세요.</h2>
       <Link href="/" className="px-4 py-2 border rounded-md">
         GO TO HOMEPAGE
       </Link>
