6ba8a59ea458bc8b9865e18af06350b5bdfaef89
oungo <y2k6273@naver.com>
2023-05-19 10:11:49 +0900

    style: add caution icon light bg

--- src/components/ticker/IconCaution.tsx
+++ src/components/ticker/IconCaution.tsx
@@ -10,7 +10,7 @@ const IconCaution = () => {
         onClick={() => setShowTooltip(!showTooltip)}
       />
       {showTooltip && (
-        <span className="absolute -top-0.5 sm:top-0 block py-0.5 px-1 text-xs rounded-md py -right-20 bg-neutral-700">
+        <span className="absolute -top-0.5 sm:top-0 block py-0.5 px-1 text-xs rounded-md py -right-20 dark:bg-neutral-700 bg-gray-200">
           투자 유의 종목
         </span>
       )}
