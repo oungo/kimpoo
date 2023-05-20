1b85153a279e6f629d7c7979ec03e185254c78dc
oungo <y2k6273@naver.com>
2023-05-20 09:14:29 +0900

    fix: fix typo

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,8 +2,7 @@ const Footer = () => (
   <footer className="relative bottom-0 w-full h-40 mt-10 bg-gray-200 dark:bg-neutral-800">
     <div className="max-w-screen-lg p-5 m-auto text-xs sm:text-sm">
       <p className="whitespace-pre-line">
-        KIMPOO에서 제공하는 정보는 투자에 대한 조언이 아닙니다.{'\n'}투자에 대한 책임은 전적으로
-        본인에게 있습니다.
+        KIMPUU에서 제공하는 정보는 투자에 대한 조언이 아닙니다.{'\n'}투자에 대한 책임은 전적으로 본인에게 있습니다.
       </p>
       <div className="mt-5">
         문의
