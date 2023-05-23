497178aa2e86f070dca5d6d1def26914a74bcf12
oungo <y2k6273@naver.com>
2023-05-23 20:17:06 +0900

    feat: show chart when clicked table row

---
+++ src/types/window.d..ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface Window {
+  TradingView: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    widget: any;
+  };
+}
