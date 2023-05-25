c3d763eda232a7b66dbc9970df95ce2809e74214
oungo <y2k6273@naver.com>
2023-05-25 22:58:23 +0900

    feat: make gtag optional

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -2,7 +2,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag('config', GA_TRACKING_ID as string, {
+  window.gtag?.('config', GA_TRACKING_ID as string, {
     page_path: url,
   });
 };
@@ -15,7 +15,7 @@ type GTagEvent = {
 };
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-  window.gtag('event', action, {
+  window.gtag?.('event', action, {
     event_category: category,
     event_label: label,
     value,
