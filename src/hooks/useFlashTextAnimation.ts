701f11847bf7636f27831e77f9b1b7e1db013c54
oungo <y2k6273@naver.com>
2023-05-30 00:41:43 +0900

    style: change flash text animation

--- src/hooks/useFlashTextAnimation.ts
+++ src/hooks/useFlashTextAnimation.ts
@@ -10,10 +10,7 @@ export const useFlashTextAnimation = () => {
       mutations.forEach((mutation) => {
         if (mutation.type === 'characterData') {
           mutation.target.parentElement?.animate(
-            [
-              { textShadow: '0px 0px 10px #ec4899', color: '#ec4899', transform: 'scale(1.1)' },
-              { textShadow: '0px 0px 10px #ec4899', color: '#ec4899', transform: 'scale(1.1)' },
-            ],
+            [{}, { textShadow: '0px 0px 10px #ec4899', color: '#ec4899', transform: 'scale(1.1)' }, {}],
             {
               duration: 300,
             }
