354ec030bfbb09c93e463921a7d7102469938053
oungo <y2k6273@naver.com>
2023-06-03 09:13:38 +0900

    feat: add metadata in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '김프 김치프리미엄(kimchi premium) - KIMPUU',
+  description: '김치프리미엄(김프) 및 암호화폐 시세를 실시간 제공합니다.',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
