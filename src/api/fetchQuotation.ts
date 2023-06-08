5c1fe57e753da641856263a8213e1893147e7af4
oungo <y2k6273@naver.com>
2023-06-09 00:19:47 +0900

    feat: apply no-store cache option

--- src/api/fetchQuotation.ts
+++ src/api/fetchQuotation.ts
@@ -3,9 +3,9 @@ export interface Quatation {
 }
 
 export const fetchQuotation = async (): Promise<Quatation> => {
-  const response = await fetch(
-    'https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD'
-  );
+  const response = await fetch('https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD', {
+    cache: 'no-store',
+  });
 
   if (response.ok) {
     const json: Quatation[] = await response.json();
