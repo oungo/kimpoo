fefe5c12cd0c6a8916261e7cbea0abc77b6eb2d7
oungo <y2k6273@naver.com>
2023-06-10 09:30:54 +0900

    fix: fix nav link

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -32,7 +32,7 @@ const Nav = () => {
   return (
     <nav className="h-20 p-2 border-b dark:border-neutral-700">
       <div className="flex items-center justify-between h-full max-w-screen-lg m-auto">
-        <Link className="text-2xl font-extrabold text-pink-500 sm:text-4xl" href="/(test)/index">
+        <Link className="text-2xl font-extrabold text-pink-500 sm:text-4xl" href="/">
           KIMPUU
         </Link>
         <button
