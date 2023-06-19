a94b183b74f26b488eff91938242fb6eaf7033bf
oungo <y2k6273@naver.com>
2023-06-19 20:10:35 +0900

    refactor: rename logError => error

--- src/api/logError.ts
+++ src/api/logError.ts
@@ -1,10 +1,10 @@
 const logError = (error: Error) => {
-  fetch('/api/logError', {
+  fetch('/api/error', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ error }),
+    body: JSON.stringify({ errorMessage: error.message }),
     // eslint-disable-next-line no-console
   }).catch((error) => console.error(`Failed to log error. ${error}`));
 };
--- src/pages/api/error.ts
+++ src/pages/api/error.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest } from 'next';
 export default function handler(req: NextApiRequest) {
   fetch(`${process.env.SLACK_WEBHOOK_URL}`, {
     method: req.method,
-    body: JSON.stringify({ text: req.body }),
+    body: JSON.stringify({ text: req.body.errorMessage }),
     headers: {
       'Content-Type': 'application/json',
     },
