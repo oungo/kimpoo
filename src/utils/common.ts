ebcff3ec888e61bf25949fc258c1555a66ed5bd9
oungo <y2k6273@naver.com>
2023-07-11 23:53:35 +0900

    refactor: change arrow function style

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,11 +1,13 @@
-export const formatNumber = (number: number, options?: Intl.NumberFormatOptions) =>
-  new Intl.NumberFormat('ko-KR', options).format(number);
+export const formatNumber = (number: number, options?: Intl.NumberFormatOptions) => {
+  return new Intl.NumberFormat('ko-KR', options).format(number);
+};
 
-export const formatCurrentPrice = (price: number) =>
-  formatNumber(price, { maximumFractionDigits: price < 1 ? 4 : price < 100 ? 2 : 0 });
+export const formatCurrentPrice = (price: number) => {
+  return formatNumber(price, { maximumFractionDigits: price < 1 ? 4 : price < 100 ? 2 : 0 });
+};
 
-export const formatPercent = (number: number) =>
-  formatNumber(number, { signDisplay: 'exceptZero', minimumFractionDigits: 2, style: 'percent' });
+export const formatPercent = (number: number) => {
+  return formatNumber(number, { signDisplay: 'exceptZero', minimumFractionDigits: 2, style: 'percent' });
+};
 
-export const createUpbitSymbolIconUrl = (symbol: string) =>
-  `https://static.upbit.com/logos/${symbol}.png`;
+export const createUpbitSymbolIconUrl = (symbol: string) => `https://static.upbit.com/logos/${symbol}.png`;
